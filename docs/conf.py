ed3365500f08faadf69d05735c5605fc371d2f8b
ariseff <ari.seff@gmail.com>
2020-07-22 12:33:44 -0400

    revert docs overwrite

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Sketchgraphs'
-copyright = '2020, Ari Seff, Yaniv Ovadia, Wenda Zhou'
-author = 'Ari Seff, Yaniv Ovadia, Wenda Zhou'
+project = 'SketchGraphs'
+copyright = '2020, Ari Seff, Yaniv Ovadia, Wenda Zhou, Ryan P. Adams'
+author = 'Ari Seff, Yaniv Ovadia, Wenda Zhou, Ryan P. Adams'
 
 
 # -- General configuration ---------------------------------------------------
